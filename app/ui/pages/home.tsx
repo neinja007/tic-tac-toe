62bb7f70581856959568daacea30e62c0e9abc60
Anton Siligan <anton.siligan@neinja.dev>
2026-05-25 20:11:50 +0200

    remove controller

--- app/ui/pages/home.tsx
+++ app/ui/pages/home.tsx
@@ -1,12 +1,10 @@
-import type { Context } from 'remix/ui';
-
-export const HomePage = (context: Context) => {
-	return context.render(
-		<div>
+export function HomePage() {
+	return () => (
+		<div className=''>
 			<h1>Welcome to Remix!</h1>
 			<p>
 				This is the home page. You can edit this page in <code>app/ui/pages/home.tsx</code>.
 			</p>
 		</div>
 	);
-};
+}
